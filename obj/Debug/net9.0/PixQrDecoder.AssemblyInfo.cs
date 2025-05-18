3b469aa607b23d9f9843ffb1ef5beb9ba834b6f9
Guilherme Valota <gui.valota@gmail.com>
2025-05-18 17:27:12 -0300

    Itens Removidos

--- obj/Debug/net9.0/PixQrDecoder.AssemblyInfo.cs
+++ obj/Debug/net9.0/PixQrDecoder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixQrDecoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57d731940765e41ea689ed6f2eaeb01231405be")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixQrDecoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixQrDecoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
